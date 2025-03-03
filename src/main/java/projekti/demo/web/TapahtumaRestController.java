76fca013b55a4fe85390f6d613606fd607c41200
Matias Nisonen <m.nisonen@gmail.com>
2025-03-03 13:34:56 +0200

    create.md luomisen välipäivitys

--- src/main/java/projekti/demo/web/TapahtumaRestController.java
+++ src/main/java/projekti/demo/web/TapahtumaRestController.java
@@ -1,5 +1,6 @@
 package projekti.demo.web;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -9,10 +10,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 
 import projekti.demo.model.Tapahtuma;
@@ -58,7 +63,8 @@ public class TapahtumaRestController {
         return tapahtumaRepository.save(muokattuTapahtuma);
     }
 
+}
     //näin jossain käytettävän public ResponseEntity<Tapahtuma> - onko tietoa olisiko tämä parempi?
     //ResponseEntity ainakin mahdollistaa paluuarvojen antamisen varmaankin paremmin
 
-}
+
