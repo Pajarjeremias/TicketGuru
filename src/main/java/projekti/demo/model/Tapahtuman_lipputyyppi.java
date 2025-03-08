6cc2e4fcccf03617725fbab9c371a37683574323
Antti Varis <ajvari88@gmail.com>
2025-03-08 20:51:40 +0200

    Lisätty LippuRestController + lisää lippu -route

--- src/main/java/projekti/demo/model/Tapahtuman_lipputyyppi.java
+++ src/main/java/projekti/demo/model/Tapahtuman_lipputyyppi.java
@@ -3,6 +3,7 @@ package projekti.demo.model;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
