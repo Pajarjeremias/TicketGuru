e6a60ff13c46830db3b53e133fe76fd13f9c0032
Antti Varis <ajvari88@gmail.com>
2025-04-25 20:04:02 +0300

    Viimeistelyt

--- src/main/client/src/types/LipunMyyntiTapahtuma.ts
+++ src/main/client/src/types/LipunMyyntiTapahtuma.ts
@@ -1,6 +1,9 @@
 import { LipunMyyntiLipputyyppi } from "./LipunMyyntiLipputyyppi";
 
 export type LipunMyyntiTapahtuma = {
+  id: number;
+  maxLiput: number;
+  currentLiput: number;
   nimi: string;
   aika: Date;
   kuvaus: string;
