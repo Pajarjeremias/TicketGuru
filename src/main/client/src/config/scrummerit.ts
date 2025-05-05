bf1caa68c0287d2b2397884cdd8934405a3592c8
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-05-05 23:27:46 +0300

    postgre ja rahti käyttöön

--- src/main/client/src/config/scrummerit.ts
+++ src/main/client/src/config/scrummerit.ts
@@ -1,6 +1,6 @@
 const config = {
-//  apiBaseUrl: "https://ticket-guru-2-ticketguru4ever2.2.rahtiapp.fi/api"
-  apiBaseUrl: "http://localhost:8080/api"
+  apiBaseUrl: "https://ticket-guru-2-ticketguru4ever2.2.rahtiapp.fi/api"
+//  apiBaseUrl: "http://localhost:8080/api"
 }
 
 export {
