c3eb43011c11384ac752cfcc8175547cb562db39
Hirvilampi <bhr611@myy.haaga-helia.fi>
2025-05-05 23:15:50 +0300

    luotapahtuma yksinkertaistettu

--- src/main/client/src/components/LuoTapahtumaComponent.tsx
+++ src/main/client/src/components/LuoTapahtumaComponent.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { config as scrummeriConfig } from "../config/scrummerit";
 
 export type TLipputyyppi = {
@@ -33,8 +33,6 @@ export default function LuoTapahtumaComponent() {
     const [message, setMessage] = useState("");
     const [uusiTapahtuma, setUusiTapahtuma] = useState<Tapahtuma | null>(null);
    // const [lipputyypit, setLipputyypit] = useState<TLipputyyppi[]>([]);
-    const [valittuTapahtumapaikka, setValittuTapahtumapaikka] = useState<number | null>(null);
-    const [tapahtumapaikat, setTapahtumapaikat] = useState<TTapahtumapaikka[]>([]);
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setPaivaMaara(e.target.value); // Päivittää valitulla arovolla
@@ -42,14 +40,15 @@ export default function LuoTapahtumaComponent() {
 
         const createTapahtuma = async () => {
             // Luodaan tapahtuma mutta tarkastetaan ensin, että jos tapahtumapaikka on valittu, lisätään se
-            const paivaMaaraISO = `${paivaMaara}:00.000+0000`;
           
             let jsondata = JSON.stringify({
                 nimi: tapahtumanNimi,
-                paivamaara: paivaMaaraISO,
+                paivamaara: paivaMaara,
                 kuvaus: tapahtumanKuvaus,
                 lippumaara: lippuMaara,
             });
+
+            console.log("TAPAHTUMAN JSON DATA: ",jsondata);
 
             try {
                 const response = await fetch(`${scrummeriConfig.apiBaseUrl}/tapahtumat`, {             
@@ -73,42 +72,6 @@ export default function LuoTapahtumaComponent() {
             }
         };
 
-
-        const fetchTapahtumapaikat = async () => {
-
-            try {
-                const response = await fetch(`${scrummeriConfig.apiBaseUrl}/tapahtumapaikat`, {
-                    headers: { 'Authorization': `Basic ${btoa('yllapitaja:yllapitaja')}` }
-                });
-                const data = await response.json();
-                if (Array.isArray(data)) {
-                    setTapahtumapaikat(data);
-                } else if (data._embedded?.tapahtumapaikat) {
-                    setTapahtumapaikat(data._embedded.tapahtumapaikat);
-                } else {
-                    throw new Error("Tapahtumapaikat puuttuvat vastauksesta");
-                }
-
-            } catch (e) {
-                console.error("API epäonnistui, lisätään testidata",e);
-                const TTpaikka: TTapahtumapaikka = {
-                    tapahtumapaikka_id: 1,
-                    nimi: "Monitoimiareena Merirosvo - testidata",
-                    katuosoite: "Areenakatu 2",
-                    postinumero: "02100",
-                    kaupunki: "Espoo",
-                    maa: "Suomi",
-                    maksimi_osallistujat: 50000,
-                    tapahtuma_id: 1,
-                }
-                setTapahtumapaikat([TTpaikka]);
-            }
-        };
-    
-
-        useEffect(() => {
-            fetchTapahtumapaikat();
-        }, []);
 
         return (
             <>
@@ -156,19 +119,6 @@ export default function LuoTapahtumaComponent() {
                             className="form-control"
                             id="maara-input">
                         </input>
-
-                        <label className="form-label">Valitse tapahtumapaikka</label>
-                        <select
-                            className="form-select"
-                            value={valittuTapahtumapaikka || ""}
-                            onChange={(e) => setValittuTapahtumapaikka(Number(e.target.value))}>
-                            <option value="">Valitse tapahtumapaikka</option>
-                            {tapahtumapaikat.map(tapahtumapaikka => (
-                                <option key={tapahtumapaikka.tapahtumapaikka_id} value={tapahtumapaikka.tapahtumapaikka_id}>
-                                    {tapahtumapaikka.nimi}
-                                </option>
-                            ))}
-                        </select>
 
                         <button
                             className="btn btn-primary mt-3"
